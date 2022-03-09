d1d2c692b18b865d4fdc9fcedd1c819e7fd64f91
Craig O'Donnell <cbodonnell@comcast.net>
2022-03-08 22:33:32 -0500

    KeyboardAvoidingView

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
-import { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { 
   StyleSheet,
-  Text,
   TextInput, 
   View,
   NativeSyntheticEvent,
-  TextInputSubmitEditingEventData
+  TextInputSubmitEditingEventData,
+  KeyboardAvoidingView,
+  Platform
 } from 'react-native';
 
 export default function App() {
@@ -13,34 +14,27 @@ export default function App() {
 
   const [input, setInput] = useState<TextInput | null>();
   const [text, setText] = useState('');
-  const [tils, setTils] = useState<string[]>(['First thing...']);
 
   useEffect(() => {
-    // write your code here, it's like componentWillMount
     input?.focus();
   }, [input])
 
   const onSubmit = (e: NativeSyntheticEvent<TextInputSubmitEditingEventData>) => {
     e.stopPropagation();
+    if (!text) return;
     console.log(`Learned: ${text}`);
-    setTils([...tils, text])
     setText('');
     input?.focus();
   }
 
   return (
-    <View style={styles.container}>
-      <View style={styles.content}>
-        <View style={styles.tils}>
-
-        {tils.map(til => 
-          <Text style={styles.til}>{til}</Text>
-        )}
-        </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}>
+      <View style={styles.inner}>
         <TextInput
           ref={el => setInput(el)}
           style={styles.input}
-          returnKeyType="next"
           placeholder="Today I learned..."
           blurOnSubmit={false}
           onChangeText={newText => setText(newText)}
@@ -48,42 +42,25 @@ export default function App() {
           onSubmitEditing={onSubmit}
         />
       </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    flex: 1
   },
-  content: {
+  inner: {
+    padding: 24,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 6,
-    width: '100%',
-    maxWidth: 1200
-  },
-  header: {
-    fontSize: 36
+    justifyContent: "space-around",
+    alignItems: 'center'
   },
   input: {
     width: '100%',
     height: 60,
     fontSize: 36,
-    textAlign: "center"
+    textAlign: "center",
+    maxWidth: 1200
   },
-  tils: {
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    zIndex: -10
-  },
-  til: {
-    fontSize: 24
-  }
 });
